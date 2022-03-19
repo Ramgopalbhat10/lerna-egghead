077e06c668e55291833a51c8c86a3ba2fd83e7ae
Subbu <ramgopalbhat10@gmail.com>
2022-03-19 21:34:05 +0530

    debug

--- packages/react-serverless/src/components/login.tsx
+++ packages/react-serverless/src/components/login.tsx
@@ -13,7 +13,7 @@ export function Login() {
 
   return (
     <div>
-      <a href="/.netlify/functions/auth" target="_blank">
+      <a href="/.netlify/functions/auth">
         <button>Login</button>
       </a>
       {/* <button onClick={getAuthInfo}>Login</button>
