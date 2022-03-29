ffa08a18b4a6ddcaf8e50b7f9d34858dddebdd56
Subbu <ramgopalbhat10@gmail.com>
2022-03-30 04:03:50 +0530

    conditional login

--- packages/react-serverless/src/components/layouts/Header.tsx
+++ packages/react-serverless/src/components/layouts/Header.tsx
@@ -1,13 +1,21 @@
 import { Header as MHeader, Button } from "@mantine/core";
 import fitbitLogo from "@assets/fitbit-icon.svg";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
-  const userId = sessionStorage.getItem("userId");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userId = sessionStorage.getItem("userId");
+    if (userId) {
+      setIsLoggedIn(true);
+    }
+  }, [isLoggedIn]);
 
   return (
     <MHeader height={75} p="md">
       <img src={fitbitLogo} alt="Fitbit logo" style={{ width: "30px" }} />
-      {!userId && (
+      {!isLoggedIn && (
         <a href="/.netlify/functions/auth">
           <Button>Login</Button>
         </a>
